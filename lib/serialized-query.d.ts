2eb9a9a3a4aa7a04424c47d87eeabb62781d7c4d
Ken Snyder <ken@ken.net>
2017-09-28 21:33:53 -0700

    README and include transpiled source

---
+++ lib/serialized-query.d.ts
@@ -0,0 +1,27 @@
+/// <reference types="firebase" />
+export interface ISimplifiedDBAdaptor {
+    ref: (path: string) => any;
+}
+export declare type LazyPath = () => string;
+export declare class SerializedQuery {
+    static path(path: string | LazyPath): SerializedQuery;
+    protected _path: string | LazyPath;
+    protected _limitToFirst: number;
+    protected _limitToLast: number;
+    protected _orderBy: 'orderByChild' | 'orderByKey' | 'orderByValue' | 'orderByValue';
+    protected _orderKey: string;
+    protected _startAt: string;
+    protected _endAt: string;
+    protected _equalTo: string;
+    constructor(path: string | LazyPath);
+    limitToFirst(num: number): this;
+    limitToLast(num: number): this;
+    orderByChild(child: string): this;
+    orderByValue(): this;
+    orderByKey(): this;
+    startAt(value: any, key?: string): this;
+    endAt(value: any, key?: string): this;
+    equalTo(value: any, key?: string): this;
+    execute(db: ISimplifiedDBAdaptor): FirebaseQuery;
+    private noKeyOnKeySort(caller, key);
+}
